2be94b6d771052e94b3649fa4283302f7d6ab420
acnavarro98 <78106037+acnavarro98@users.noreply.github.com>
2021-03-10 13:32:35 -0800

    lab 12

--- lab12/js/lab12.js
+++ lab12/js/lab12.js
@@ -5,22 +5,62 @@
  */
 
 // Create a "FizzBuzz" function similar to how we did in class.
-  function fizzBuzz (maxNums, factorObj) {
-
+  //function fizzBuzz () {
   // Loop through numbers 1 to 200, listing them as you go
-    for (var num=0; num < maxNums; num++) {
-      
+    for (var num = 1; num <= 200; num++) {
+
+    // Make it so the output of your program appears in your output div.
+      outputEl = document.getElementById("output");
+      newEl = document.createElement("p");
+
+    //If the number is a multiple of 3, 5 and 7: print "FizzBuzzBoom!"
+      if (num % 105 == 0) {
+        newEl.innerHTML = num + " - FizzBuzzBoom!";
+        outputEl.appendChild(newEl);
+        console.log("FizzBuzzBoom!");
+      }
+      //If the number is a multiple of 5 and 7: print "BoomBuzz!"
+        else if (num % 35 == 0) {
+          newEl.innerHTML = num + " - BoomBuzz!";
+          outputEl.appendChild(newEl);
+          console.log("BoomBuzz!");
+      }
+      //If the number is a multiple of 3 and 7: print "FizzBoom!"
+        else if (num % 21 == 0) {
+          newEl.innerHTML = num + " - FizzBoom!";
+          outputEl.appendChild(newEl);
+          console.log("FizzBoom!");
+      }
+      //If the number is a multiple of 3 and 5: print "FizzBuzz!"
+        else if (num % 15 == 0) {
+          newEl.innerHTML = num + " - FizzBuzz!";
+          outputEl.appendChild(newEl);
+          console.log("FizzBuzz!");
+      }
+
+      // If the number is a multiple of 3, if should print "Fizz!"
+        else if (num % 3 == 0) {
+          newEl.innerHTML = num + " - Fizz!";
+          outputEl.appendChild(newEl);
+          console.log("Fizz!");
+      }
+      // If the number is a multiple of 5, it should print "Buzz!"
+        else if (num % 5 == 0) {
+          newEl.innerHTML = num + " - Buzz!";
+          outputEl.appendChild(newEl);
+          console.log("Buzz!");
+      }
+      // If the number is a multiple of 7, it should print "Boom!"
+        else if (num % 7 == 0) {
+          newEl.innerHTML = num + " - Boom!";
+          outputEl.appendChild(newEl);
+          console.log("Boom!");
+      }
+        else {
+          newEl.innerHTML = num;
+          outputEl.appendChild(newEl);
+          console.log(num);
+      }
     }
-// If the number is a multiple of 3, if should print "Fizz!"
-
-// If the number is a multiple of 5, it should print "Buzz!"
-
-// If the number is a multiple of 7, it should print "Boom!"
-
-// If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on.
-
-// Make it so the output of your program appears in your output div.
-  // One way to do this is to compile one long string as you output numbers. Something like: str += num + " Fizz!<br>"; Then output the str at the end to your div using innerHTML.
-  // Another way: For each number, create a new DOM element, change the innerHTML to the text you want, and appendChild() to the end of your output div.
+// }
 //Don't forget to call your function.
-}
